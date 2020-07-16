dc402f61cddcf2cf3aa3097fad9623446b012e9c
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-16 14:01:47 -0400

    added schema.sql and seeds.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,28 @@
--- -- Drops the todolist if it exists currently --
--- DROP DATABASE IF EXISTS test_db;
--- -- Creates the "todolist" database --
--- CREATE DATABASE test_db;
+DROP DATABASE IF EXISTS monsterra_db;
+
+CREATE DATABASE monsterra_db;
+
+
+CREATE TABLE forest (
+  id int NOT NULL AUTO_INCREMENT,
+  pet_name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE water (
+  id int NOT NULL AUTO_INCREMENT,
+  pet_name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE grass (
+  id int NOT NULL AUTO_INCREMENT,
+  pet_name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rocks (
+  id int NOT NULL AUTO_INCREMENT,
+  pet_name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
